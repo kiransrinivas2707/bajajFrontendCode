1b2e209aa65b34bb08c29376b12793240220949a
kiransrinivas2707 <141928312+kiransrinivas2707@users.noreply.github.com>
2024-08-25 14:25:53 +0530

    Update App.js
    backend url update

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
       console.log("Parsed JSON Data:", parsedData);
   
       // Call the REST API
-      const res = await axios.post('http://localhost:3000/bfhl', parsedData); // Corrected URL
+      const res = await axios.post('https://bajajbackend-1-mbeu.onrender.com/bfhl', parsedData); // Corrected URL
       setResponse(res.data);
       setShowDropdown(true);
     } catch (err) {
